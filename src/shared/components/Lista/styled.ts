2225d3f15378aa1e3949511eacd24dd74fa0aa23
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2022-11-15 10:50:09 -0400

    :lipstick: feat: Estilizaçao CSS Techs

--- src/shared/components/Lista/styled.ts
+++ src/shared/components/Lista/styled.ts
@@ -1,25 +1,31 @@
 import styled from "styled-components";
 
 export const ContainerList = styled.div`
-  width: 50.3rem;
-  height: 10.3rem;
+  width: 53rem;
+  height: 13rem;
   background: #27272a;
-  border: 1px solid #9d4b00;
+  border: 2px solid #9d4b00;
   border-radius: 8px;
   display: flex;
-  justify-content: space-around;
+  /* justify-content: space-around; */
   align-items: center;
-  padding: 2rem 4.6rem;
+  padding: 3rem;
 
   ul {
     display: flex;
     flex-direction: column;
-    width: auto;
+    width: 100%;
     height: 100%;
     align-items: flex-start;
+    padding: 0 2rem;
+    flex-wrap: wrap;
+    list-style: none;
 
     li {
-      font-size: 1.2rem;
+      font-size: 1.3rem;
+      color: rgba(255, 255, 255, 0.87);
+      margin: 0 1rem;
+      padding-left: 1rem;
     }
   }
 `;
